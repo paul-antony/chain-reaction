5ccd8db5085467ecf70c9a3da20b201b21e4d8c6
paul-antony <paulantonytoc@gmail.com>
2018-09-22 06:03:15 +0000

    Update CR_minimax.py tested each function

--- Chain Reaction - Minimax/CR_minimax.py
+++ Chain Reaction - Minimax/CR_minimax.py
@@ -10,7 +10,7 @@
 #
 
 
-class Board():
+class Board:
 	#
 	#
 	# Function Name:    __init__()
@@ -23,11 +23,11 @@ class Board():
 	# Example Call:     Board.__init__(9, 6, 1)
 	#
 	#
-	def __init__(self, m=9, n=6, player=1):
-		self.m = m
-		self.n = n
-		self.board = [[0 for i in xrange(self.n)] for i in xrange(self.m)]
-		self.player = player
+        def __init__(self, m=9, n=6, player=1):
+                self.m = m
+                self.n = n
+                self.board = [[0 for i in range(self.n)] for i in range(self.m)]
+                self.player = player
 
 	#
 	#
@@ -39,8 +39,8 @@ class Board():
 	# Example Call:     self.__getitem__([0,1])
 	#
 	#
-	def __getitem__(self, pos):
-		return self.board[pos[0]][pos[1]]
+        def __getitem__(self, pos):
+                return self.board[pos[0]][pos[1]]
 
 	#
 	#
@@ -54,56 +54,35 @@ class Board():
 	# Example Call:     self.__getitem__([0,1])
 	#
 	#
-	def __setitem__(self, pos, value):
-		self.board[pos[0]][pos[1]]=value
-	def __str__(self):
-		s = ""
-		for i in xrange(self.m):
-			for j in xrange(self.n):
-				s += str(self[(i,j)])
-				s += " "
-			s += "\n"
-		return s
-	def hash(self):
-		return str(self.board)+str(self.new_move)
-	def critical_mass(self,pos):
-		if pos == (0,0) or pos == (self.m - 1, self.n - 1) or pos == (self.m - 1, 0) or pos == (0, self.n - 1):
-			return 2
-		elif pos[0] == 0 or pos[0] == self.m-1 or pos[1] == 0 or pos[1] == self.n-1:
-			return 3
-		else:
-			return 4
-	def neighbors(self,pos):
-		n = []
-		for i in [(pos[0],pos[1]+1), (pos[0],pos[1]-1), (pos[0]+1,pos[1]), (pos[0]-1,pos[1])]:
-			if 0 <= i[0] < self.m and 0 <= i[1] < self.n:
-				n.append(i)
-		return n
-	def move(board, pos):
-		board = copy.deepcopy(board)
-		assert board.new_move == sgn(board[pos]) or 0 == sgn(board[pos])
-		board[pos] = board[pos] + board.new_move
-		t = time.time()
-		while True:
-			unstable = []
-			for pos in [(x,y) for x in xrange(board.m) for y in xrange(board.n)]:
-				if abs(board[pos]) >= board.critical_mass(pos):
-					unstable.append(pos)
-			if time.time() - t >= 3:
-				#Can't afford to spend more time, strange loop here!
-				#print board, pos
-				#raw_input()
-				break
-			#print board
-			#raw_input()
-			if not unstable:
-				break
-			for pos in unstable:
-				board[pos] -= board.new_move*board.critical_mass(pos)
-				for i in board.neighbors(pos):
-					board[i] = sgn(board.new_move)*(abs(board[i])+1)
-		board.new_move *= -1
-		return board
-	def input(self):
-		
+        def __setitem__(self, pos, value):
+                self.board[pos[0]][pos[1]]=value
+                
+        def __str__(self):
+                s = ""
+                for i in range(self.m):
+                        for j in range(self.n):
+                                s += str(self[(i,j)])
+                                s += " "
+                        s += "\n"
+                return s
+        
 
+
+        def critical_mass(self,pos):
+                if pos == (0,0) or pos == (self.m - 1, self.n - 1) or pos == (self.m - 1, 0) or pos == (0, self.n - 1):
+                        return 2
+                elif pos[0] == 0 or pos[0] == self.m-1 or pos[1] == 0 or pos[1] == self.n-1:
+                        return 3
+                else:
+                        return 4
+
+        def neighbors(self,pos):
+                n = []
+                for i in [(pos[0],pos[1]+1), (pos[0],pos[1]-1), (pos[0]+1,pos[1]), (pos[0]-1,pos[1])]:
+                        if 0 <= i[0] < self.m and 0 <= i[1] < self.n:
+                                n.append(i)
+                return n
+
+        def input(self,state):
+                for pos in [(x,y) for x in range(self.m) for y in range(self.n)]:
+                        self.board[pos[0]][pos[1]] = state[pos[0]][pos[1]]
