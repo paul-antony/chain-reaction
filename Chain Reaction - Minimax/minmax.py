e46e39d491b07160dca05c94e1fa6766ab0a8ecd
paul-antony <paulantonytoc@gmail.com>
2018-10-02 08:46:22 +0000

    Update minmax.py split it into max and min function

--- Chain Reaction - Minimax/minmax.py
+++ Chain Reaction - Minimax/minmax.py
@@ -3,34 +3,44 @@ import copy
 
 
 def minmax(b,depth=3):
+        return max_value(b,depth)
 
-        moves = b.valid_move()
-        if len(moves) == 0:
-                return b.cal_heuristics(),0
-        if depth == 1:
-                return b.cal_heuristics(),0
-
-    
-        if b.player == 1:
-                best_val = -300
-                best_move = moves[0]
-                for pos in moves:
-                        board = copy.deepcopy(b)
-                        board.move(pos)
-                        b_score = minmax(board,depth-1)[0]
-                        if best_val < b_score:
-                                best_val = b_score
-                                best_move = pos
-                return best_val,best_move
-    
-        if b.player == -1:
-                best_val = 300
-                best_move = moves[0]
-                for pos in moves:
-                        board = copy.deepcopy(b)
-                        board.move(pos)
-                        b_score = minmax(board,depth-1)[0]
-                        if best_val > b_score:
-                                best_val = b_score
-                                best_move = pos
-                return best_val,best_move 
+def max_value(b,depth):
+    moves = b.valid_move()
+    if len(moves) == 0:
+        return -1*b.cal_heuristics(),0
+    if depth == 0:
+        return -1*b.cal_heuristics(),0
+                        
+    best_val = -300
+    best_move = moves[0]
+    for pos in moves:
+        board = copy.deepcopy(b)
+        board.move(pos)
+        b_score = min_value(board,depth-1)[0]
+        
+        if best_val < b_score:
+            best_val = b_score
+            best_move = pos
+            
+    return best_val,best_move
+                        
+def min_value(b,depth):
+    moves = b.valid_move()
+    if len(moves) == 0:
+        return -1*b.cal_heuristics(),0
+    if depth == 0:
+        return -1*b.cal_heuristics(),0
+                        
+    best_val = 300
+    best_move = moves[0]
+    for pos in moves:
+        board = copy.deepcopy(b)
+        board.move(pos)
+        b_score = max_value(board,depth-1)[0]
+        
+        if best_val > b_score:
+            best_val = b_score
+            best_move = pos
+            
+    return best_val,best_move
