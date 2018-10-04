e87de272e46809420f13ad165319e4075c102861
paul-antony <paulantonytoc@gmail.com>
2018-10-04 16:02:53 +0000

    Replace test.py

--- Chain Reaction - Minimax/test.py
+++ Chain Reaction - Minimax/test.py
@@ -1,29 +1,30 @@
 from board import *
-from alpha_beta import *
+from minmax import *
 
 
 
 
 def test():
-        b=Board()
-        #print(b)
-        #b.input(((0,0,0,0,0,0),(0,0,0,0,0,0),(0,0,0,1,-1,0),(0,0,2,-3,3,-1),(0,-1,-3,3,-3,0),(0,0,1,3,0,1),(0,0,-1,-1,0,0),(0,0,0,0,0,0),(0,0,0,0,0,0)))
-        #print(b)
-        #b.move((4,3))
-        #print(b)
-        #print(b.player)
-        #print(b.valid_move())
-        #b.move((0,0))
-        #print(b)
-        #print(b.cal_heuristics())
-        #b.player *= -1
-        #print(b.cal_heuristics())
-        b.input(((0,0,0,0,0,0),(0,0,0,0,0,0),(0,0,0,0,0,0),(0,0,0,0,0,0),(0,0,3,3,0,0),(0,0,-3,-3,0,0),(0,0,0,0,0,0),(0,0,0,0,0,0),(0,0,0,0,0,0)))
-        #print(b.player)
-        print(alpha_beta(b,4))
-        #b.move((4,2))
-        #print(b)
-        #print(b.valid_move())
-        #print(b.cal_heuristics())
-        
+	b=Board()
+	#print(b)
+	#b.input(((0,0,0,0,0,0),(0,0,0,0,0,0),(0,0,0,1,-1,0),(0,0,2,-3,3,-1),(0,-1,-3,3,-3,0),(0,0,1,3,0,1),(0,0,-1,-1,0,0),(0,0,0,0,0,0),(0,0,0,0,0,0)))
+	#print(b)
+	#b.move((4,3))
+	#print(b)
+	#print(b.player)
+	#print(b.valid_move())
+	#b.move((0,0))
+	#print(b)
+	#print(b.cal_heuristics())
+	#b.player *= -1
+	print("Present State:")
+	b.input(((0,0,0,0,0,0),(0,0,0,0,0,0),(0,0,0,0,0,0),(0,0,0,0,0,0),(0,0,3,3,0,0),(0,0,-3,-3,0,0),(0,0,0,0,0,0),(0,0,0,0,0,0),(0,0,0,0,0,0)))
+	print(b)
+	print("Best Move:",minmax(b,3)[1])
+	#print(b)
+	#print(b.valid_move())
+	#print(b.cal_heuristics())
+	#b.move((4,2))
+
+
 test()
