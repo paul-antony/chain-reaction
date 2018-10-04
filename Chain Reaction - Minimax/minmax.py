088f5320e4f201b63234002b0f3efda4de964cc2
paul-antony <paulantonytoc@gmail.com>
2018-10-04 16:02:13 +0000

    Replace minmax.py

--- Chain Reaction - Minimax/minmax.py
+++ Chain Reaction - Minimax/minmax.py
@@ -1,47 +1,44 @@
-#minmax implementation
-
 import copy
 
-
 def minmax(b,depth=3):
-        return max_value(b,depth)
+	return max_value(b,depth)
 
 def max_value(b,depth):
-    moves = b.valid_move()
-    if len(moves) == 0:
-        return -1*b.cal_heuristics(),0
-    if depth == 0:
-        return -1*b.cal_heuristics(),0
+	moves = b.valid_move()
+	if len(moves) == 0:
+		return -1*b.cal_heuristics(),0
+	if depth == 0:
+		return -1*b.cal_heuristics(),0
                         
-    best_val = -300
-    best_move = moves[0]
-    for pos in moves:
-        board = copy.deepcopy(b)
-        board.move(pos)
-        b_score = min_value(board,depth-1)[0]
+	best_val = -300
+	best_move = moves[0]
+	for pos in moves:
+		board = copy.deepcopy(b)
+		board.move(pos)
+		b_score = min_value(board,depth-1)[0]
         
-        if best_val < b_score:
-            best_val = b_score
-            best_move = pos
+		if best_val < b_score:
+			best_val = b_score
+			best_move = pos
             
-    return best_val,best_move
+	return best_val,best_move
                         
 def min_value(b,depth):
-    moves = b.valid_move()
-    if len(moves) == 0:
-        return -1*b.cal_heuristics(),0
-    if depth == 0:
-        return -1*b.cal_heuristics(),0
+	moves = b.valid_move()
+	if len(moves) == 0:
+		return -1*b.cal_heuristics(),0
+	if depth == 0:
+		return -1*b.cal_heuristics(),0
                         
-    best_val = 300
-    best_move = moves[0]
-    for pos in moves:
-        board = copy.deepcopy(b)
-        board.move(pos)
-        b_score = max_value(board,depth-1)[0]
+	best_val = 300
+	best_move = moves[0]
+	for pos in moves:
+		board = copy.deepcopy(b)
+		board.move(pos)
+		b_score = max_value(board,depth-1)[0]
         
-        if best_val > b_score:
-            best_val = b_score
-            best_move = pos
+	if best_val > b_score:
+		best_val = b_score
+		best_move = pos
             
-    return best_val,best_move
+	return best_val,best_move
