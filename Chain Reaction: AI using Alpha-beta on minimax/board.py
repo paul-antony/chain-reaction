2a6758d759bfdc092c8b74c541b531fa041c4b1a
jobjacob98 <jobjacob98@gmail.com>
2018-10-10 21:46:24 +0530

    Almost done with Alpha-beta

---
+++ Chain Reaction: AI using Alpha-beta on minimax/board.py
@@ -0,0 +1,196 @@
+#
+#
+# Task Name:        Implementation of MiniMax algorithm in Chain Reaction
+# Author List:      Job Jacob, Paul Antony
+# File Name:        board.py
+# Functions:
+#
+# Global variables:
+#
+#
+
+
+
+
+class Board():
+        #
+        #
+        # Function Name:    __init__()
+        # Input:            1) self --> the object of the class
+        #                   2) m --> the number of rows for the board
+        #                   3) n --> the number of columns for the board
+        #                   4) player --> used to determine which player's turn it is at any particular instance
+        # Output:           None
+        # Logic:            This function is used to initialize the Board object variables.
+        # Example Call:     Board.__init__(9, 6, 1)
+        #
+        #
+        def __init__(self, m=9, n=6, player=1):
+                self.m = m
+                self.n = n
+                self.board = [[0 for i in range(self.n)] for i in range(self.m)]
+                self.player = player
+
+        #
+        #
+        # Function Name:    __getitem__()
+        # Input:            1) self --> the object of the class
+        #                   2) pos --> the position (index) of any cell in the m*n board
+        # Output:           self.board[pos[0]][pos[1]] --> the number of orbs present in the given cell position
+        # Logic:            This function is used to return the number of orbs in a particular cell.
+        # Example Call:     self.__getitem__([0,1])
+        #
+        #
+        def __getitem__(self, pos):
+                return self.board[pos[0]][pos[1]]
+
+        #
+        #
+        # Function Name:    __setitem__()
+        # Input:            1) self --> the object of the class
+        #                   2) pos --> the position (index) of any cell in the m*n board
+        #                   3) value --> the number of orbs present in the given cell position
+        # Output:           None
+        # Logic:            This function is used to assign the number of orbs to the board position.
+        # Example Call:     self.__setitem__([0,1], 2)
+        #
+        #
+        def __setitem__(self, pos, value):
+                self.board[pos[0]][pos[1]] = value
+
+        #
+        #
+        # Function Name:    __str__()
+        # Input:            self --> the object of the class
+        # Output:           state --> the current state of the board
+        # Logic:            This function is used to return the current state of the board (as a string).
+        # Example Call:     self.__str__()
+        #
+        #
+        def __str__(self):
+                state = ""
+                for i in range(self.m):
+                        for j in range(self.n):
+                                state += str(self.board[i][j])
+                                state += " "
+                        state += "\n"
+                return state
+
+        #
+        #
+        # Function Name:    criticalMass()
+        # Input:            1) self --> the object of the class
+        #                   2) pos --> the position (index) of any cell in the m*n board
+        # Output:           1) 2 --> the critical mass of the given cell if it is a corner cell
+        #                   2) 3 --> the critical mass of the given cell if it is an edge cell
+        #                   3) 4 --> the critical mass of the given cell if it is a cell inside the board
+        # Logic:            This function is used to return the critical mass of a particular cell. If the cell is a corner, then the critical mass of that cell is 2.
+        #                                       If it is an edge cell, then the critical mass of that cell is 3. And if it is a cell inside the board, then the critical mass of that cell
+        #                                       is 4.
+        # Example Call:     self.criticalMass([0,1])
+        #
+        #
+        def critical_mass(self, pos):
+                if pos == (0, 0) or pos == (self.m-1, self.n-1) or pos == (self.m-1, 0) or pos == (0, self.n-1):
+                        return 2
+                elif pos[0] == 0 or pos[0] == self.m-1 or pos[1] == 0 or pos[1] == self.n-1:
+                        return 3
+                else:
+                        return 4
+
+        #
+        #
+        # Function Name:    neighbors()
+        # Input:            1) self --> the object of the class
+        #                                       2) pos --> the position (index) of any cell in the m*n board
+        # Output:           neighborsList --> the list of neighbors of the given cell
+        # Logic:            This function is used to return the list of orthogonally adjacent neighbors of a particular cell.
+        # Example Call:     self.neighbors([0,1])
+        #
+        #
+        def neighbors(self, pos):
+                neighbors_list = []
+                for i in [(pos[0],pos[1]+1), (pos[0],pos[1]-1), (pos[0]+1,pos[1]), (pos[0]-1,pos[1])]:
+                        if 0 <= i[0] < self.m and 0 <= i[1] < self.n:
+                                neighbors_list.append(i)
+                return neighbors_list
+
+
+
+
+
+        def input(self, state):
+                for pos in [(x,y) for x in range(self.m) for y in range(self.n)]:
+                        self.board[pos[0]][pos[1]] = state[pos[0]][pos[1]]
+
+
+
+
+        #
+        #
+        # Function Name:    valid_move()
+        # Input:            self --> the object of the class
+        # Output:           valid --> the list of moves valid for a player
+        # Logic:            This function is used to find and return the list of valid moves a player can take at any given state.
+        # Example Call:     self.valid_move()
+        #
+        #
+        def valid_move(self):
+                if self.cal_heuristics() in [200,-200]:
+                        return []
+                valid = []
+                for pos in [(x,y) for x in range(self.m) for y in range(self.n)]:
+                        if self.board[pos[0]][pos[1]]/self.player >= 0:
+                                valid.append(pos)
+                return valid
+
+
+        #
+        #
+        # Function Name:    move()
+        # Input:            1) self --> the object of the class
+        #                   2) pos --> the position (index) of any cell in the m*n board
+        # Output:                       None
+        # Logic:            This function is used to execute a move by us. This includes putting an orb at the given cell and also the resulting chain reaction if the
+        #                                       cell has reached its critical mass.
+        # Example Call:     self.valid_move()
+        #
+        #
+        def move(self, pos):
+                self.board[pos[0]][pos[1]] += self.player
+                unstable = []
+                unstable.append(pos)
+                for pos in unstable:
+                        if abs(self.board[pos[0]][pos[1]]) >= self.critical_mass(pos):
+                                self.board[pos[0]][pos[1]] -= self.player * self.critical_mass(pos)
+                                for i in self.neighbors(pos):
+                                        self.board[i[0]][i[1]] = self.player * (abs(self.board[i[0]][i[1]]) + 1)
+                                        unstable.append(i)
+                self.player *= -1
+                return self
+
+        #
+        #
+        # Function Name:    cal_heuristics()
+        # Input:            self --> the object of the class
+        # Output:           heuristicValue --> the heuristic value in a given state
+        # Logic:            This function is used to find and return the heuristic value at a particular state of the board. For finding the heuristic value, we take
+        #                                       into account the number of our orbs present on the board, the number of our opponent's orbs present on the board, our orb cells that are
+        #                                       about to reach their critical mass and our opponent neighboring cells that are about to reach their critical mass.
+        # Example Call:     self.neighbors([0,1])
+        #
+        #
+        def cal_heuristics(self):
+                heuristic_value = 0
+                my_orbs, opponent_orbs = 0, 0
+                for pos in [(x,y) for x in range(self.m) for y in range(self.n)]:
+                        if self.board[pos[0]][pos[1]]/self.player > 0:
+                                my_orbs += abs(self.board[pos[0]][pos[1]])
+                        else:
+                                opponent_orbs += abs(self.board[pos[0]][pos[1]])
+                heuristic_value = my_orbs - opponent_orbs
+                if opponent_orbs == 0 and my_orbs > 1:
+                        return 200
+                elif my_orbs == 0 and opponent_orbs > 1:
+                        return -200
+                return heuristic_value
---
+++ Chain Reaction: AI using Alpha-beta on minimax/game.py
@@ -0,0 +1,159 @@
+import pygame
+from alpha_beta import *
+from board import Board
+import _thread as thread
+
+m, n = 9, 6
+
+pygame.init()
+surface = pygame.display.set_mode((50*n, 50*m))
+pygame.display.set_caption('Chain Reaction')
+lock = thread.allocate_lock()
+
+def drawBoard(board_ui):
+	surface.fill((0,0,0))
+	font = pygame.font.Font('Font.ttf', 48)
+	for pos in [(x,y) for x in range(board_ui.m) for y in range(board_ui.n)]:
+		if abs(board_ui.board[pos[0]][pos[1]]) >= board_ui.critical_mass(pos):
+			color = (255,255,0)
+		elif board_ui.board[pos[0]][pos[1]]/board_ui.player > 0:
+			color = (90,90,90)
+		elif board_ui.board[pos[0]][pos[1]]/board_ui.player < 0:
+			color = (255,0,0)
+		else:
+			color = (0,255,0)
+		text = font.render(str(board_ui.board[pos[0]][pos[1]])[-1], 1, color)
+		textpos = text.get_rect(centerx = pos[1]*50 + 25, centery = pos[0]*50 + 25)
+		surface.blit(text, textpos)
+	pygame.display.update()
+
+def slowMove(board_ui, pos):
+	while True:
+		drawBoard(board_ui)
+		pygame.time.wait(250)
+		unstable = []
+		for pos in [(x,y) for x in range(board_ui.m) for y in range(board_ui.n)]:
+			if abs(board_ui.board[pos[0]][pos[1]]) >= board_ui.critical_mass(pos):
+				unstable.append(pos)
+		#raw_input()
+		if not unstable:
+			break
+		for pos in unstable:
+			board_ui.board[pos] -= board_ui.board.player*board_ui.board.critical_mass(pos)
+			for i in board.neighbors(pos):
+				board_ui.board[i] = board_ui.board[pos[0]][pos[1]]/board_ui.board.player*(abs(board_ui.board[i])+1)
+	drawBoard(board_ui)
+	lock.release()
+
+def show_move(pos):
+	rect = pygame.Rect(pos[1]*50,pos[0]*50,50,50)
+	pygame.draw.rect(surface,(255,255,0),rect,0)
+	pygame.display.update()
+	pygame.time.wait(250)
+
+
+def main():
+	global m,n, surface
+
+	#start screen
+	font = pygame.font.Font('Font.ttf', 72)
+	text = font.render("Red", 1, (255,0,0))
+	textpos = text.get_rect(centerx = 25*n, centery = 12*m)
+	surface.blit(text, textpos)
+	text = font.render("Green", 1, (0,255,0))
+	textpos = text.get_rect(centerx = 25*n, centery = 36*m)
+	surface.blit(text, textpos)
+	font = pygame.font.Font('Font.ttf', 12)
+	text = font.render("Choose a Color", 1, (100,100,100))
+	textpos = text.get_rect(centerx = 25*n, centery = 25*m)
+	surface.blit(text, textpos)
+	pygame.display.update()
+
+	this_loop = True
+	while this_loop:
+		for event in pygame.event.get():
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				y = pygame.mouse.get_pos()[1]
+				if y < 25*m:
+					player_first = True
+				else:
+					player_first = False
+				this_loop = False
+
+
+
+	#some initialization code
+	m, n = 9, 6
+	surface = pygame.display.set_mode((50*n, 50*m))
+	pygame.display.set_caption('Chain Reaction')
+	board_ui = Board(m=m,n=n)
+	total_moves = 0
+
+	#game screen
+	drawBoard(board_ui)
+
+	if not player_first:
+		new_move = alpha_beta(board_ui,4)
+		lock.acquire()
+		thread.start_new_thread(slowMove, (board_ui, new_move))
+		board_ui = board_ui.move(new_move)
+		total_moves += 1
+
+	this_loop = True
+	while this_loop:
+		for event in pygame.event.get():
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				x,y = pygame.mouse.get_pos()
+				pos = (int(y/50), int(x/50))
+				print(pos)
+				#if not (board_ui.player == board_ui.board[pos[0]][pos[1]]/board_ui.player or 0 == board_ui.board[pos[0]][pos[1]]/board_ui.player):
+				print("player = " ,board_ui.player)
+				if board_ui.board[pos[0]][pos[1]]/board_ui.player < 0:
+					print("Illegal Move!")
+					continue
+				show_move(pos)
+				lock.acquire()
+				thread.start_new_thread(slowMove, (board_ui, pos, ))
+				board_move = board_ui.move(pos)
+				total_moves += 1
+				if total_moves >= 2:
+					if board_ui.cal_heuristics() == 200:
+						winner = board_ui.player*(-1)
+						this_loop = False
+						break
+				new_move = alpha_beta(board_ui, 4)
+				print("new_move ", new_move)
+				print(new_move)
+				show_move(new_move)
+				lock.acquire()
+				thread.start_new_thread(slowMove, (board_ui, new_move, ))
+				board = board_ui.move(new_move)
+				total_moves += 1
+				if total_moves >= 2:
+					if board_ui.cal_heuristics() == 200:
+						winner = board_ui.player*(-1)
+						this_loop = False
+						break
+
+	#winning screen
+	while lock.locked():
+		continue
+	m, n = 9, 6
+	surface = pygame.display.set_mode((50*n, 50*m))
+	font = pygame.font.Font('Font.ttf', 72)
+	pygame.display.set_caption('Chain Reaction')
+	if winner == 1:
+		text = font.render("Red", 1, (255,0,0))
+	else:
+		text = font.render("Green", 1, (0,255,0))
+	textpos = text.get_rect(centerx = 25*n, centery = 12*m)
+	surface.blit(text, textpos)
+	font = pygame.font.Font('Font.ttf', 48)
+	text = font.render("Wins!", 1, (100,100,100))
+	textpos = text.get_rect(centerx = 25*n, centery = 25*m)
+	surface.blit(text, textpos)
+	pygame.display.update()
+
+
+if __name__ == "__main__":
+	main()
