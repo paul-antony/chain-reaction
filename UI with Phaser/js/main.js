cebf0c189bd2619fa16fe06b022cf1c1de612ac4
jobjacob98 <jobjacob98@gmail.com>
2018-10-18 01:02:36 +0530

    Phaser Board Created

--- UI with Phaser/js/main.js
+++ UI with Phaser/js/main.js
@@ -1,5 +1,3 @@
-var game;
-
 var board = [
   [
     [0, 0, 0, 0, 0, 0],
@@ -15,52 +13,42 @@ var board = [
 ];
 
 var gameOptions = {
-  gameWidth: 700,
-  gameHeight: 1050,
+  gameWidth: 397,
+  gameHeight: 592,
   cellSize: 100,
   boardSize: {
     rows: 9,
     cols: 6
   },
-  directions: [
-    new Phaser.Point(0, 1),
-    new Phaser.Point(0, -1),
-    new Phaser.Point(1, 0),
-    new Phaser.Point(-1, 0),
-  ]
+  colors: [0xff0000, 0x00ff00],
 }
 
-window.onload = function() {
-  game = new Phaser.Game(gameOptions.gameWidth, gameOptions.gameHeight);
-  game.state.add("TheGame", TheGame);
-  game.state.start("TheGame");
+var config = {
+  type: Phaser.AUTO,
+  width: gameOptions.gameWidth,
+  height: gameOptions.gameHeight,
+  parent: 'game-section',
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
 }
 
-var TheGame = function() {};
+var game = new Phaser.Game(config);
 
-TheGame.prototype = {
-  preload: function() {
-    game.stage.backgroundColor = 0x2e3131;
-  },
-  create: function() {
-    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-    game.scale.pageAlignHorizontally = true;
-    game.scale.pageAlignVertically = true;
-    this.createBoard();
-  },
-  createBoard: function() {
-    this.cellsArray = [];
-    this.cellGroup = game.add.group();
-    this.cellGroup.x = (game.width - gameOptions.cellSize * gameOptions.boardSize.cols) / 2;
-    this.cellGroup.y = (game.height - gameOptions.cellSize * gameOptions.boardSize.rows) / 2;
-    for(var i = 0; i < gameOptions.boardSize.rows; i++) {
-      this.cellsArray[i] = [];
-      for(var j = 0; j < gameOptions.boardSize.cols; j++) {
-        this.addCell(i, j);
-      }
+function preload() {
+  this.load.image('cell', 'assets/cell.png');
+}
+
+function create() {
+  for(let i=0; i<=520; i=i+65) {
+    for(let j=0; j<=325; j=j+65) {
+      this.add.image(j+7, i+7, 'cell').setOrigin(0, 0);
     }
-    game.input.onDown.add(this.pickCell, this);
-  },
-  addCell: function(row, col) {
-    var cellXPos = col * gameOptions.cellSize + gameOptions.cellSize / 2;
-    var cellYPos = row * gameOptions.cellSize + gameOptions.cellSize / 2;
+  }
+}
+
+function update() {
+
+}
