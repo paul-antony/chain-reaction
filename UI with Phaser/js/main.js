ddf450d99f01909859253ee0b2ecfe9fd4dfd1bf
jobjacob98 <jobjacob98@gmail.com>
2018-10-19 23:57:50 +0530

    redid UI using Phaser 2 instead of Phaser 3

--- UI with Phaser/js/main.js
+++ UI with Phaser/js/main.js
@@ -1,5 +1,6 @@
-var board = [
-  [
+var gameState = {
+  player: 1,
+  board: [
     [0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0],
@@ -10,45 +11,112 @@ var board = [
     [0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0]
   ]
-];
+}
 
 var gameOptions = {
-  gameWidth: 397,
-  gameHeight: 592,
-  cellSize: 100,
+  gameWidth: 362,
+  gameHeight: 542,
   boardSize: {
     rows: 9,
     cols: 6
   },
-  colors: [0xff0000, 0x00ff00],
+  cellSize: 58,
+  cellPadding: 2,
+  orbRadius: 20,
+  colors: [0xff0000, 0x00ff00]
 }
 
-var config = {
-  type: Phaser.AUTO,
-  width: gameOptions.gameWidth,
-  height: gameOptions.gameHeight,
-  parent: 'game-section',
-  scene: {
-    preload: preload,
-    create: create,
-    update: update
-  }
-}
+var cell = [[]];
 
-var game = new Phaser.Game(config);
+var game = new Phaser.Game(gameOptions.gameWidth, gameOptions.gameHeight, Phaser.AUTO, 'game-section', { preload: preload, create: create });
 
 function preload() {
   this.load.image('cell', 'assets/cell.png');
+  game.stage.backgroundColor = gameOptions.colors[0];
 }
 
 function create() {
-  for(let i=0; i<=520; i=i+65) {
-    for(let j=0; j<=325; j=j+65) {
-      this.add.image(j+7, i+7, 'cell').setOrigin(0, 0);
+  for(let yPos=0; yPos<(gameOptions.gameHeight-gameOptions.cellPadding); yPos+=(gameOptions.cellPadding+gameOptions.cellSize)) {
+    for(let xPos=0; xPos<(gameOptions.gameWidth-gameOptions.cellPadding); xPos+=(gameOptions.cellPadding+gameOptions.cellSize)) {
+      game.add.image((xPos+gameOptions.cellPadding), (yPos+gameOptions.cellPadding), 'cell');
     }
+  }
+  game.input.onDown.add(updateGameState, this);
+}
+
+function updateGameState() {
+  let xClickedPos = game.input.mousePointer.x;
+  let yClickedPos = game.input.mousePointer.y;
+  let flag = -1;
+  let rowIndex = 0;
+  for(let yPos=0; yPos<(gameOptions.gameHeight-gameOptions.cellPadding); yPos+=(gameOptions.cellPadding+gameOptions.cellSize)) {
+    let colIndex = 0;
+    for(let xPos=0; xPos<(gameOptions.gameWidth-gameOptions.cellPadding); xPos+=(gameOptions.cellPadding+gameOptions.cellSize)) {
+      if(xClickedPos>=(xPos+gameOptions.cellPadding) && xClickedPos<=(xPos+gameOptions.cellPadding+gameOptions.cellSize) && yClickedPos>=(yPos+gameOptions.cellPadding) && yClickedPos<=(yPos+gameOptions.cellPadding+gameOptions.cellSize)) {
+        if(gameState.player==1 && gameState.board[rowIndex][colIndex]>=0) {
+          gameState.board[rowIndex][colIndex]++;
+          flag = 0;
+        }
+        else if(gameState.player==-1 && gameState.board[rowIndex][colIndex]<=0) {
+          gameState.board[rowIndex][colIndex]--;
+          flag = 0;
+        }
+        else {
+          flag = 1;
+        }
+        checkBurst(rowIndex, colIndex);
+      }
+      colIndex++;
+    }
+    rowIndex++;
+  }
+  if(flag==0) {
+      updateBoard();
+      gameState.player *= -1;
+      if(gameState.player==1) {
+        game.stage.backgroundColor = gameOptions.colors[0];
+      }
+      else {
+        game.stage.backgroundColor = gameOptions.colors[1];
+      }
+  }
+  console.log(gameState.board);
+}
+
+function checkBurst(rowIndex, colIndex) {
+  if(((rowIndex==0 && colIndex==0) || (rowIndex==0 && colIndex==(gameState.board[rowIndex].length-1)) || (rowIndex==(gameState.board.length-1) && colIndex==0) || (rowIndex==(gameState.board[rowIndex].length-1) && colIndex==(gameState.board.length-1))) && (gameState.board[rowIndex][colIndex]==2 || gameState.board[rowIndex][colIndex]==-2)) {
+
+  }
+  else if((rowIndex==0 || colIndex==0 || rowIndex==(gameState.board.length-1) || colIndex==(gameState.board[rowIndex].length-1)) && (gameState.board[rowIndex][colIndex]==3 || gameState.board[rowIndex][colIndex]==-3)) {
+
+  }
+  else if(gameState.board[rowIndex][colIndex]==4 || gameState.board[rowIndex][colIndex]==-4) {
+
   }
 }
 
-function update() {
-
+function updateBoard() {
+  var graphics = game.add.graphics(0, 0);
+  for(let rowIndex=0; rowIndex<gameState.board.length; ++rowIndex) {
+    for(let colIndex=0; colIndex<gameState.board[colIndex].length; ++colIndex) {
+      if(gameState.board[rowIndex][colIndex]>0) {
+        graphics.beginFill(gameOptions.colors[0], 1);
+        switch(gameState.board[rowIndex][colIndex]) {
+          case 4: graphics.drawCircle((gameOptions.cellPadding+gameOptions.cellSize)*(colIndex)+gameOptions.cellPadding+39, (gameOptions.cellPadding+gameOptions.cellSize)*(rowIndex)+gameOptions.cellPadding+39, gameOptions.orbRadius);
+          case 3: graphics.drawCircle((gameOptions.cellPadding+gameOptions.cellSize)*(colIndex)+gameOptions.cellPadding+19, (gameOptions.cellPadding+gameOptions.cellSize)*(rowIndex)+gameOptions.cellPadding+39, gameOptions.orbRadius);
+          case 2: graphics.drawCircle((gameOptions.cellPadding+gameOptions.cellSize)*(colIndex)+gameOptions.cellPadding+39, (gameOptions.cellPadding+gameOptions.cellSize)*(rowIndex)+gameOptions.cellPadding+19, gameOptions.orbRadius);
+          case 1: graphics.drawCircle((gameOptions.cellPadding+gameOptions.cellSize)*(colIndex)+gameOptions.cellPadding+19, (gameOptions.cellPadding+gameOptions.cellSize)*(rowIndex)+gameOptions.cellPadding+19, gameOptions.orbRadius);
+        }
+      }
+      else if(gameState.board[rowIndex][colIndex]<0) {
+        graphics.beginFill(gameOptions.colors[1], 1);
+        switch(gameState.board[rowIndex][colIndex]) {
+          case -4: graphics.drawCircle((gameOptions.cellPadding+gameOptions.cellSize)*(colIndex)+gameOptions.cellPadding+39, (gameOptions.cellPadding+gameOptions.cellSize)*(rowIndex)+gameOptions.cellPadding+39, gameOptions.orbRadius);
+          case -3: graphics.drawCircle((gameOptions.cellPadding+gameOptions.cellSize)*(colIndex)+gameOptions.cellPadding+19, (gameOptions.cellPadding+gameOptions.cellSize)*(rowIndex)+gameOptions.cellPadding+39, gameOptions.orbRadius);
+          case -2: graphics.drawCircle((gameOptions.cellPadding+gameOptions.cellSize)*(colIndex)+gameOptions.cellPadding+39, (gameOptions.cellPadding+gameOptions.cellSize)*(rowIndex)+gameOptions.cellPadding+19, gameOptions.orbRadius);
+          case -1: graphics.drawCircle((gameOptions.cellPadding+gameOptions.cellSize)*(colIndex)+gameOptions.cellPadding+19, (gameOptions.cellPadding+gameOptions.cellSize)*(rowIndex)+gameOptions.cellPadding+19, gameOptions.orbRadius);
+        }
+      }
+    }
+  }
 }
