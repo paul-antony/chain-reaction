2a5b4621be8b283d78b7a604be9b30bc82315b7b
Job Jacob <jobjacob98@gmail.com>
2018-10-27 03:01:13 +0000

    Update board.py

--- Alpha_beta_Chain_Reaction/board.py
+++ Alpha_beta_Chain_Reaction/board.py
@@ -16,57 +16,57 @@ class Board():
 	#
 	#
 	# Function Name:    __init__()
-        # Input:            1) self --> the object of the class
-        #                   2) m --> the number of rows for the board
-        #                   3) n --> the number of columns for the board
-        #                   4) player --> used to determine which player's turn it is at any particular instance
-        # Output:           None
-        # Logic:            This function is used to initialize the Board object variables. 
-        # Example Call:     Board.__init__(9, 6, 1)
-        #
-        #
+    # Input:            1) self --> the object of the class
+    #                   2) m --> the number of rows for the board
+    #                   3) n --> the number of columns for the board
+    #                   4) player --> used to determine which player's turn it is at any particular instance
+    # Output:           None
+    # Logic:            This function is used to initialize the Board object variables. 
+    # Example Call:     Board.__init__(9, 6, 1)
+    #
+    #
 	def __init__(self, m=9, n=6, player=1):
 		self.m = m
 		self.n = n
 		self.board = [[0 for i in range(self.n)] for i in range(self.m)]
 		self.player = player
 
-        #
-        #
-        # Function Name:    __getitem__()
-        # Input:            1) self --> the object of the class
-        #                   2) pos --> the position (index) of any cell in the m*n board
-        # Output:           self.board[pos[0]][pos[1]] --> the number of orbs present in the given cell position
-        # Logic:            This function is used to return the number of orbs in a particular cell.
-        # Example Call:     self.__getitem__([0,1])
-        #
-        #
+    #
+    #
+    # Function Name:    __getitem__()
+    # Input:            1) self --> the object of the class
+    #                   2) pos --> the position (index) of any cell in the m*n board
+    # Output:           self.board[pos[0]][pos[1]] --> the number of orbs present in the given cell position
+    # Logic:            This function is used to return the number of orbs in a particular cell.
+    # Example Call:     self.__getitem__([0,1])
+    #
+    #
 	def __getitem__(self, pos):
 		return self.board[pos[0]][pos[1]]
 
-        #
-        #
-        # Function Name:    __setitem__()
-        # Input:            1) self --> the object of the class
-        #                   2) pos --> the position (index) of any cell in the m*n board
-        #                   3) value --> the number of orbs present in the given cell position
-        # Output:           None
-        # Logic:            This function is used to assign the number of orbs to the board position.
-        # Example Call:     self.__setitem__([0,1], 2)
-        #
-        #
+    #
+    #
+    # Function Name:    __setitem__()
+    # Input:            1) self --> the object of the class
+    #                   2) pos --> the position (index) of any cell in the m*n board
+    #                   3) value --> the number of orbs present in the given cell position
+    # Output:           None
+    # Logic:            This function is used to assign the number of orbs to the board position.
+    # Example Call:     self.__setitem__([0,1], 2)
+    #
+    #
 	def __setitem__(self, pos, value):
 		self.board[pos[0]][pos[1]] = value
 
-        #
-        #
-        # Function Name:    __str__()
-        # Input:            self --> the object of the class
-        # Output:           state --> the current state of the board
-        # Logic:            This function is used to return the current state of the board (as a string).
-        # Example Call:     self.__str__()
-        #
-        #
+    #
+    #
+    # Function Name:    __str__()
+    # Input:            self --> the object of the class
+    # Output:           state --> the current state of the board
+    # Logic:            This function is used to return the current state of the board (as a string).
+    # Example Call:     self.__str__()
+    #
+    #
 	def __str__(self):
 		state = ""
 		for i in range(self.m):
@@ -76,20 +76,20 @@ class Board():
 			state += "\n"
 		return state
 
-        #
-        #
-        # Function Name:    criticalMass()
-        # Input:            1) self --> the object of the class 
-        #                   2) pos --> the position (index) of any cell in the m*n board
-        # Output:           1) 2 --> the critical mass of the given cell if it is a corner cell
-        #                   2) 3 --> the critical mass of the given cell if it is an edge cell
-        #                   3) 4 --> the critical mass of the given cell if it is a cell inside the board
-        # Logic:            This function is used to return the critical mass of a particular cell. If the cell is a corner, then the critical mass of that cell is 2. 
-        #                                       If it is an edge cell, then the critical mass of that cell is 3. And if it is a cell inside the board, then the critical mass of that cell 
-        #                                       is 4.
-        # Example Call:     self.criticalMass([0,1])
-        #
-        #
+    #
+    #
+    # Function Name:    criticalMass()
+    # Input:            1) self --> the object of the class 
+    #                   2) pos --> the position (index) of any cell in the m*n board
+    # Output:           1) 2 --> the critical mass of the given cell if it is a corner cell
+    #                   2) 3 --> the critical mass of the given cell if it is an edge cell
+    #                   3) 4 --> the critical mass of the given cell if it is a cell inside the board
+    # Logic:            This function is used to return the critical mass of a particular cell. If the cell is a corner, then the critical mass of that cell is 2. 
+    #                   If it is an edge cell, then the critical mass of that cell is 3. And if it is a cell inside the board, then the critical mass of that cell 
+    #                   is 4.
+    # Example Call:     self.criticalMass([0,1])
+    #
+    #
 	def critical_mass(self, pos):
 		if pos == (0, 0) or pos == (self.m-1, self.n-1) or pos == (self.m-1, 0) or pos == (0, self.n-1):
 			return 2
@@ -98,16 +98,16 @@ class Board():
 		else:
 			return 4
 
-        #
-        #
-        # Function Name:    neighbors()
-        # Input:            1) self --> the object of the class 
-        #                                       2) pos --> the position (index) of any cell in the m*n board
-        # Output:           neighborsList --> the list of neighbors of the given cell
-        # Logic:            This function is used to return the list of orthogonally adjacent neighbors of a particular cell.
-        # Example Call:     self.neighbors([0,1])
-        #
-        #
+    #
+    #
+    # Function Name:    neighbors()
+    # Input:            1) self --> the object of the class 
+    #                   2) pos --> the position (index) of any cell in the m*n board
+    # Output:           neighborsList --> the list of neighbors of the given cell
+    # Logic:            This function is used to return the list of orthogonally adjacent neighbors of a particular cell.
+    # Example Call:     self.neighbors([0,1])
+    #
+    #
 	def neighbors(self, pos):
 		neighbors_list = []
 		for i in [(pos[0],pos[1]+1), (pos[0],pos[1]-1), (pos[0]+1,pos[1]), (pos[0]-1,pos[1])]:
@@ -126,15 +126,15 @@ class Board():
 
 
 
-        #
-        #
-        # Function Name:    valid_move()
-        # Input:            self --> the object of the class 
-        # Output:           valid --> the list of moves valid for a player
-        # Logic:            This function is used to find and return the list of valid moves a player can take at any given state.
-        # Example Call:     self.valid_move()
-        #
-        #             
+    #
+    #
+    # Function Name:    valid_move()
+    # Input:            self --> the object of the class 
+    # Output:           valid --> the list of moves valid for a player
+    # Logic:            This function is used to find and return the list of valid moves a player can take at any given state.
+    # Example Call:     self.valid_move()
+    #
+    #             
 	def valid_move(self):
 		if self.cal_heuristics() in [200,-200]:
 			return []
@@ -144,17 +144,17 @@ class Board():
 				valid.append(pos)
 		return valid
 
-        #
-        #
-        # Function Name:    move()
-        # Input:            1) self --> the object of the class 
-        #                   2) pos --> the position (index) of any cell in the m*n board
-        # Output:                       None
-        # Logic:            This function is used to execute a move by us. This includes putting an orb at the given cell and also the resulting chain reaction if the 
-        #                                       cell has reached its critical mass.
-        # Example Call:     self.valid_move()
-        #
-        #     
+    #
+    #
+    # Function Name:    move()
+    # Input:            1) self --> the object of the class 
+    #                   2) pos --> the position (index) of any cell in the m*n board
+    # Output:           None
+    # Logic:            This function is used to execute a move by us. This includes putting an orb at the given cell and also the resulting chain reaction if the 
+    #                   cell has reached its critical mass.
+    # Example Call:     self.valid_move()
+    #
+    #     
 	def move(self, pos):
 		self.board[pos[0]][pos[1]] += self.player
 		unstable = []
@@ -170,17 +170,17 @@ class Board():
 					unstable.append(i)
 		self.player *= -1      
 
-        #
-        #
-        # Function Name:    cal_heuristics()
-        # Input:            self --> the object of the class 
-        # Output:           heuristicValue --> the heuristic value in a given state
-        # Logic:            This function is used to find and return the heuristic value at a particular state of the board. For finding the heuristic value, we take 
-        #                                       into account the number of our orbs present on the board, the number of our opponent's orbs present on the board, our orb cells that are
-        #                                       about to reach their critical mass and our opponent neighboring cells that are about to reach their critical mass.  
-        # Example Call:     self.neighbors([0,1])
-        #
-        #
+    #
+    #
+    # Function Name:    cal_heuristics()
+    # Input:            self --> the object of the class 
+    # Output:           heuristicValue --> the heuristic value in a given state
+    # Logic:            This function is used to find and return the heuristic value at a particular state of the board. For finding the heuristic value, we take 
+    #                   into account the number of our orbs present on the board, the number of our opponent's orbs present on the board, our orb cells that are
+    #                   about to reach their critical mass and our opponent neighboring cells that are about to reach their critical mass.  
+    # Example Call:     self.neighbors([0,1])
+    #
+    #
 	def cal_heuristics(self):
 		heuristic_value = 0
 		my_orbs, opponent_orbs = 0, 0
