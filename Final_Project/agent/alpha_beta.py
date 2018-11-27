f19daffb36af0626c8ba06be7b335c7c1b8301c3
paul-antony <paulantonytoc@gmail.com>
2018-11-27 08:00:42 +0530

    commit

--- Final_Project/agent/alpha_beta.py
+++ Final_Project/agent/alpha_beta.py
@@ -12,22 +12,12 @@ def alpha_beta(b,depth = 2):
 
 	b_score = -300
 	best_move = moves[0]
-<<<<<<< HEAD:Final Chain Reaction/agent1/alpha_beta.py
-    
-	for pos in moves:
-		board = copy.deepcopy(b)
-		board.move(pos)
-		b_score = max(b_score,min_value(board,depth-1,alpha,beta,player))
-
-    
-=======
 
 	for pos in moves:
 		board = copy.deepcopy(b)
 		board.move(pos)
 		b_score = max(b_score, min_value(board, depth-1, alpha,beta, player))
 
->>>>>>> final task:Final Chain Reaction/agent/alpha_beta.py
 		if alpha < b_score:
 			alpha = b_score
 			best_move = pos
@@ -37,15 +27,7 @@ def alpha_beta(b,depth = 2):
 
 	return best_move
 
-<<<<<<< HEAD:Final Chain Reaction/agent1/alpha_beta.py
-
-
-
-
-def max_value(b,depth,alpha,beta,player):
-=======
 def max_value(b, depth, alpha, beta, player):
->>>>>>> final task:Final Chain Reaction/agent/alpha_beta.py
 	moves = b.valid_move()
 
 	if len(moves) == 0:
@@ -54,16 +36,6 @@ def max_value(b, depth, alpha, beta, player):
 		return b.cal_heuristics(player)
 
 	b_score = -300
-<<<<<<< HEAD:Final Chain Reaction/agent1/alpha_beta.py
-    
-	for pos in moves:
-		board = copy.deepcopy(b)
-		board.move(pos)
-		b_score = max(b_score,min_value(board,depth-1,alpha,beta,player))
-
-    
-		alpha = max(alpha,b_score)
-=======
 
 	for pos in moves:
 		board = copy.deepcopy(b)
@@ -71,18 +43,12 @@ def max_value(b, depth, alpha, beta, player):
 		b_score = max(b_score, min_value(board, depth-1, alpha, beta, player))
 
 		alpha = max(alpha, b_score)
->>>>>>> final task:Final Chain Reaction/agent/alpha_beta.py
 
 		if alpha >= beta:
 			break
 
 	return b_score
 
-<<<<<<< HEAD:Final Chain Reaction/agent1/alpha_beta.py
-
-
-=======
->>>>>>> final task:Final Chain Reaction/agent/alpha_beta.py
 def min_value(b,depth,alpha,beta,player):
 	moves = b.valid_move()
 
@@ -92,15 +58,6 @@ def min_value(b,depth,alpha,beta,player):
 		return b.cal_heuristics(player)
 
 	b_score = 300
-<<<<<<< HEAD:Final Chain Reaction/agent1/alpha_beta.py
-    
-	for pos in moves:
-		board = copy.deepcopy(b)
-		board.move(pos)
-		b_score = min(b_score,max_value(board,depth-1,alpha,beta,player))
-
-		beta = min(beta,b_score)
-=======
 
 	for pos in moves:
 		board = copy.deepcopy(b)
@@ -108,7 +65,6 @@ def min_value(b,depth,alpha,beta,player):
 		b_score = min(b_score, max_value(board, depth-1, alpha, beta, player))
 
 		beta = min(beta, b_score)
->>>>>>> final task:Final Chain Reaction/agent/alpha_beta.py
 
 		if alpha >= beta:
 			break
--- Final_Project/api.py
+++ Final_Project/api.py
@@ -33,39 +33,6 @@ def main():
 	game_mode = int(request.form['mode'])
 	agent_turn = int(request.form['player'])
 	cur_board = json.loads(request.form['board'], encoding="utf-8")
-<<<<<<< HEAD
-	print(game_mode, file=sys.stderr)
-	print(agent_turn, file=sys.stderr)
-	print(cur_board, file=sys.stderr)
-
-	board.input(cur_board,agent_turn)
-
-	if game_mode == 2:
-		return json.dumps(list(alpha_beta(board,4)))
-	
-	elif game_mode == 3:
-		#network = QNetwork()
-		#network.model._make_predict_function()
-		if board.player == 1:
-			file_name = 'agent2/player1.h5'
-		else:
-			file_name = 'agent2/player2.h5'
-		with network.graph[0].as_default():
-			action = network.act(board,file_name = file_name)
-			action=[int(action[0]),int(action[1])]
-
-
-		return json.dumps(action)
-
-
-	elif game_mode == 4:
-		agent1_turn = request.form['agent1']
-		print(agent1_turn, file=sys.stderr)
-		if (int(agent1_turn)==1 and int(agent_turn==1)) or (int(agent1_turn)==-1 and int(agent_turn)==-1):
-			return json.dumps(list(alpha_beta(board,4)))
-		elif (int(agent1_turn)==1 and int(agent_turn)==-1) or (int(agent1_turn)==-1 and int(agent_turn)==1):
-			return json.dumps(list(alpha_beta(board,4)))
-=======
 
 	board.input(cur_board, agent_turn)
 
@@ -97,6 +64,5 @@ def main():
 				action = [int(action[0]), int(action[1])]
 			time.sleep(1)
 			return json.dumps(action)
->>>>>>> final task
 
 app.run()
